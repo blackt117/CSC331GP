f547aea5f1acdc0308a83b164aa17b61c264abe6
Tyler Black <145217103+blackt117@users.noreply.github.com>
2023-11-10 14:17:50 -0500

    Changes

--- Scripts/GameDriver.java
+++ Scripts/GameDriver.java
@@ -1,2 +1,16 @@
+/**
+ * @Author Tyler Black, Marjorie Pare, Aaron Brown, Emma Burger, Jameson Baltzer
+ * @Purpose
+ * @Date
+ * @Section
+ */
+import java.util.Scanner;
 public class GameDriver {
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to our game");
+        System.out.println("Please enter a character you would like to use");
+        Scanner input = new Scanner(System.in);
+        String player1 = input.nextLine();
+    }
 }
