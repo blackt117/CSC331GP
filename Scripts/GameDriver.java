47604f71df0bf2e030bf8997c0ac4e939f3ef546
Jb5698 <149415575+Jb5698@users.noreply.github.com>
2023-11-13 13:03:16 -0500

    Update GameDriver.java

--- Scripts/GameDriver.java
+++ Scripts/GameDriver.java
@@ -8,9 +8,15 @@ import java.util.Scanner;
 public class GameDriver {
     public static void main (String[] args)
     {
-        System.out.println("Welcome to our game");
-        System.out.println("Please enter a character you would like to use");
         Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to our game");
+        System.out.println("Would you like to play a new game? (Y for yes N for no) ")
+        String answer = input.nextLine();
+        if (answer.equals("Y") || answer.equals("y"){
+            //start game
+        }
+        System.out.println("Please enter a character you would like to use");
+        
         String player1 = input.nextLine();
     }
 }
